582d8b9656e925987624fa222e5b8f8ccbd61c3a
LuluLudmi <gilmaaguadajura@gmail.com>
2026-01-29 13:57:55 -0300

    Diseño completo: logo, css y estructura html de Zona Refri

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.cta-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('¡Producto añadido al carrito! ❄️ El dueño de Zona Refri ya está preparando tu pedido.');
+    });
+});
